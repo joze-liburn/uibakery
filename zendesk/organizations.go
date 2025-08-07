dd48d6609d52a179760603c2ae772777d20d94e3
joze-liburn <joze@lightburnsoftware.com>
2025-08-07 08:41:57 +0200

    File renamed, unit test added.

---
+++ zendesk/organizations.go
@@ -0,0 +1,115 @@
+package zendesk
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+// https://developer.zendesk.com/api-reference/ticketing/organizations/organizations/
+type (
+	OrganizationFields struct {
+		MakerspaceDiscountCode string `json:"makerspace_discount_code"`
+		OrgReseller            bool   `json:"org_reseller"`
+		ResellerDiscountCode   string `json:"reseller_discount_code"`
+		SyncShopifyCompany     bool   `json:"sync_shopify_company"`
+		Other                  map[string]any
+	}
+	Organization struct {
+		CreatedAt          *time.Time          `json:"created_at"`
+		Details            string              `json:"details"`
+		DomainNames        []string            `json:"domain_names"`
+		ExternalId         string              `json:"external_id"`
+		GroupId            int64               `json:"group_id"`
+		Id                 int64               `json:"id"`
+		Name               string              `json:"name"`
+		Notes              string              `json:"notes"`
+		OrganizationFields *OrganizationFields `json:"organization_fields"`
+		SharedComments     bool                `json:"shared_comments"`
+		SharedTickets      bool                `json:"shared_tickets"`
+		Tags               []string            `json:"tags"`
+		UpdatedAt          *time.Time          `json:"updated_at"`
+		Url                string              `json:"url"`
+	}
+	OrganizationResult struct {
+		Organizations []Organization `json:"organizations"`
+		Meta          Meta           `json:"meta"`
+		Links         Links          `json:"links"`
+	}
+)
+
+func jsonToOrganizations(js []byte) (OrganizationResult, error) {
+	var or OrganizationResult
+	if err := json.Unmarshal(js, &or); err != nil {
+		return OrganizationResult{}, err
+	}
+	return or, nil
+}
+
+func (zd *Zendesk) GetOrganizations(page int) (OrganizationResult, error) {
+	opts := []GetOptions{}
+	if page > 0 {
+		opts = append(opts, WithPage(page))
+	}
+	rsp, err := zd.Get("organizations", opts...)
+	if err != nil {
+		return OrganizationResult{}, err
+	}
+	return jsonToOrganizations(rsp)
+}
+
+type OrganizationError struct {
+	Organization Organization
+	Err          error
+}
+
+// StreamOrganizations provides a channel with successive Organizations. Every
+// organization in the channel has non-trivial ExternalId.
+func (zd *Zendesk) StreamOrganizations(pageSize int, maxCount uint) <-chan OrganizationError {
+	opts := []GetOptions{}
+	if pageSize > 0 {
+		opts = append(opts, WithPage(pageSize))
+	}
+	out := make(chan OrganizationError)
+	go func() {
+		defer close(out)
+		var count uint
+		rsp, geterr := zd.Get("organizations", opts...)
+		for {
+			if geterr != nil {
+				out <- OrganizationError{Err: geterr}
+				return
+			}
+			or, err := jsonToOrganizations(rsp)
+			if err != nil {
+				fmt.Printf("----- %s -----\n", zd.zdApi)
+				out <- OrganizationError{Err: err}
+				return
+			}
+			for _, org := range or.Organizations {
+				if len(org.ExternalId) == 0 {
+					continue
+				}
+				out <- OrganizationError{Organization: org}
+				count++
+				if count >= maxCount {
+					return
+				}
+			}
+			if !or.Meta.HasMore {
+				return
+			}
+			rsp, geterr = zd.Get("organizations", append(opts, StartAfter(or.Meta.AfterCursor))...)
+		}
+	}()
+	return out
+}
+
+func (zd *Zendesk) GetOrganizationsByExternalId(extId string, page int) (OrganizationResult, error) {
+	//	zd := NewZendesk(ZendeskApi, "574a0f524e9d4fb15bc6f678cf67f11ef442cd285d62c6b8f28397a996b7d37a")
+	rsp, err := zd.Get("organizations")
+	if err != nil {
+		return OrganizationResult{}, err
+	}
+	return jsonToOrganizations(rsp)
+}
---
+++ zendesk/organizations_test.go
@@ -0,0 +1,340 @@
+package zendesk
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+const (
+	api_organizations_organizations = `"organizations":[
+        {
+            "url":"https://lightburnsoftware.zendesk.com/api/v2/organizations/1.json",
+            "id":1,
+            "name":"LightBurn Software",
+            "shared_tickets":true,
+            "shared_comments":true,
+            "external_id":null,
+            "created_at":"2023-09-19T16:26:39Z",
+            "updated_at":"2024-05-01T16:40:10Z",
+            "domain_names":["digifable.com","lightburnsoftware.com","millmage.com"],
+            "details":"Staff Organization",
+            "notes":"",
+            "group_id":11,
+            "tags":["lightburn-staff-email"],
+            "organization_fields":{
+                "makerspace_discount_code":null,
+                "org_reseller":false,
+                "reseller_discount_code":null,
+                "sync_shopify_company":false
+            }
+        },
+        {
+            "url":"https://lightburnsoftware.zendesk.com/api/v2/organizations/2.json",
+            "id":2,
+            "name":"3D Flying Bear",
+            "shared_tickets":false,
+            "shared_comments":false,
+            "external_id":null,
+            "created_at":"2023-11-17T21:32:53Z",
+            "updated_at":"2024-02-08T22:05:24Z",
+            "domain_names":["3dflyingbear.com"],
+            "details":"",
+            "notes":"",
+            "group_id":11,
+            "tags":["reseller","vendor"],
+            "organization_fields":{
+                "makerspace_discount_code":null,
+                "org_reseller":true,
+                "reseller_discount_code":"Fly1ngB3@r-Di$tro",
+                "sync_shopify_company":false
+            }
+        }
+    ]`
+	api_organizations_meta = `"meta": {
+        "has_more":true,
+        "after_cursor":"eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0=",
+        "before_cursor":"eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0="
+    }`
+	api_organizations_links = `"links":{
+        "prev":"https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bbefore%5D=eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0%3D&page%5Bsize%5D=2",
+        "next":"https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bafter%5D=eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0%3D&page%5Bsize%5D=2"
+    }`
+)
+
+func Time(year int, mon time.Month, day, h, m, s int, loc *time.Location) *time.Time {
+	tm := time.Date(year, mon, day, h, m, s, 0, loc)
+	return &tm
+}
+
+func TestGetOrganizations(t *testing.T) {
+	tests := []struct {
+		name string
+		data string
+		want OrganizationResult
+	}{
+		{
+			name: "normal",
+			data: "{" + api_organizations_organizations + "," + api_organizations_meta + "," + api_organizations_links + "}",
+			want: OrganizationResult{
+				Organizations: []Organization{
+					{
+						Url:                "https://lightburnsoftware.zendesk.com/api/v2/organizations/1.json",
+						Id:                 1,
+						Name:               "LightBurn Software",
+						SharedTickets:      true,
+						SharedComments:     true,
+						CreatedAt:          Time(2023, time.September, 19, 16, 26, 39, time.UTC),
+						UpdatedAt:          Time(2024, time.May, 1, 16, 40, 10, time.UTC),
+						DomainNames:        []string{"digifable.com", "lightburnsoftware.com", "millmage.com"},
+						Details:            "Staff Organization",
+						Notes:              "",
+						GroupId:            11,
+						Tags:               []string{"lightburn-staff-email"},
+						OrganizationFields: &OrganizationFields{},
+					},
+					{
+						Url:            "https://lightburnsoftware.zendesk.com/api/v2/organizations/2.json",
+						Id:             2,
+						Name:           "3D Flying Bear",
+						SharedTickets:  false,
+						SharedComments: false,
+						CreatedAt:      Time(2023, time.November, 17, 21, 32, 53, time.UTC),
+						UpdatedAt:      Time(2024, time.February, 8, 22, 05, 24, time.UTC),
+						DomainNames:    []string{"3dflyingbear.com"},
+						Details:        "",
+						Notes:          "",
+						GroupId:        11,
+						Tags:           []string{"reseller", "vendor"},
+						OrganizationFields: &OrganizationFields{
+							OrgReseller:          true,
+							ResellerDiscountCode: "Fly1ngB3@r-Di$tro",
+						},
+					},
+				},
+				Meta: Meta{
+					HasMore:      true,
+					BeforeCursor: "eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0=",
+					AfterCursor:  "eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0=",
+				},
+				Links: Links{
+					Prev: "https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bbefore%5D=eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0%3D&page%5Bsize%5D=2",
+					Next: "https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bafter%5D=eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0%3D&page%5Bsize%5D=2",
+				},
+			},
+		},
+		{
+			name: "organizations",
+			data: "{" + api_organizations_organizations + "}",
+			want: OrganizationResult{
+				Organizations: []Organization{
+					{
+						Url:                "https://lightburnsoftware.zendesk.com/api/v2/organizations/1.json",
+						Id:                 1,
+						Name:               "LightBurn Software",
+						SharedTickets:      true,
+						SharedComments:     true,
+						CreatedAt:          Time(2023, time.September, 19, 16, 26, 39, time.UTC),
+						UpdatedAt:          Time(2024, time.May, 1, 16, 40, 10, time.UTC),
+						DomainNames:        []string{"digifable.com", "lightburnsoftware.com", "millmage.com"},
+						Details:            "Staff Organization",
+						Notes:              "",
+						GroupId:            11,
+						Tags:               []string{"lightburn-staff-email"},
+						OrganizationFields: &OrganizationFields{},
+					},
+					{
+						Url:            "https://lightburnsoftware.zendesk.com/api/v2/organizations/2.json",
+						Id:             2,
+						Name:           "3D Flying Bear",
+						SharedTickets:  false,
+						SharedComments: false,
+						CreatedAt:      Time(2023, time.November, 17, 21, 32, 53, time.UTC),
+						UpdatedAt:      Time(2024, time.February, 8, 22, 05, 24, time.UTC),
+						DomainNames:    []string{"3dflyingbear.com"},
+						Details:        "",
+						Notes:          "",
+						GroupId:        11,
+						Tags:           []string{"reseller", "vendor"},
+						OrganizationFields: &OrganizationFields{
+							OrgReseller:          true,
+							ResellerDiscountCode: "Fly1ngB3@r-Di$tro",
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "meta",
+			data: "{" + api_organizations_meta + "}",
+			want: OrganizationResult{
+				Meta: Meta{
+					HasMore:      true,
+					BeforeCursor: "eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0=",
+					AfterCursor:  "eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0=",
+				},
+			},
+		},
+		{
+			name: "links",
+			data: "{" + api_organizations_links + "}",
+			want: OrganizationResult{
+				Links: Links{
+					Prev: "https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bbefore%5D=eyJvIjoiaWQiLCJ2IjoiYVJ2OUd6VTBFUUFBIn0%3D&page%5Bsize%5D=2",
+					Next: "https://lightburnsoftware.zendesk.com/api/v2/organizations.json?page%5Bafter%5D=eyJvIjoiaWQiLCJ2IjoiYVJzYTdacWlFZ0FBIn0%3D&page%5Bsize%5D=2",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := jsonToOrganizations([]byte(test.data))
+			if err != nil {
+				t.Errorf("%s: got error %s", test.name, err)
+			}
+			if df := cmp.Diff(test.want, got); df != "" {
+				t.Errorf("%s: -want +got\n%s", test.name, df)
+			}
+		})
+	}
+}
+
+func timep(t time.Time) *time.Time {
+	return &t
+}
+
+func TestJsonToOrganizations(t *testing.T) {
+	jsonstr := `
+{
+  "organizations": [
+    {
+      "url": "https://lightburnsoftware.zendesk.com/api/v2/organizations/33340257215003.json",
+      "id": 33340257215003,
+      "name": "(TRANSLASER) JR ACESSORIOS LTDA",
+      "shared_tickets": true,
+      "shared_comments": false,
+      "external_id": "gid://shopify/Company/1661599912",
+      "created_at": "2025-01-29T16:55:02Z",
+      "updated_at": "2025-07-19T11:32:13Z",
+      "domain_names": [
+        "translaser.com.br"
+      ],
+      "details": null,
+      "notes": null,
+      "group_id": 20488928016667,
+      "tags": [
+        "vendor",
+        "reseller"
+      ],
+      "organization_fields": {
+        "makerspace_discount_code": null,
+        "org_reseller": false,
+        "reseller_discount_code": null,
+        "sync_shopify_company": false
+      }
+    },
+    {
+      "url": "https://lightburnsoftware.zendesk.com/api/v2/organizations/20489593231899.json",
+      "id": 20489593231899,
+      "name": "3D Flying Bear",
+      "shared_tickets": false,
+      "shared_comments": false,
+      "external_id": null,
+      "created_at": "2023-11-17T21:32:53Z",
+      "updated_at": "2024-02-08T22:05:24Z",
+      "domain_names": [
+        "3dflyingbear.com"
+      ],
+      "details": "",
+      "notes": "",
+      "group_id": 20488928016667,
+      "tags": [
+        "reseller",
+        "vendor"
+      ],
+      "organization_fields": {
+        "makerspace_discount_code": null,
+        "org_reseller": true,
+        "reseller_discount_code": "Fly1ngB3@r-Di$tro",
+        "sync_shopify_company": false
+      }
+    }
+  ]
+}`
+	tests := []struct {
+		name    string
+		data    string
+		want    OrganizationResult
+		wanterr bool
+	}{
+		{
+			name: "2",
+			data: jsonstr,
+			want: OrganizationResult{
+				Organizations: []Organization{
+					{
+						Url:            "https://lightburnsoftware.zendesk.com/api/v2/organizations/33340257215003.json",
+						Id:             33340257215003,
+						Name:           "(TRANSLASER) JR ACESSORIOS LTDA",
+						SharedTickets:  true,
+						SharedComments: false,
+						ExternalId:     "gid://shopify/Company/1661599912",
+						CreatedAt:      timep(time.Date(2025, 1, 29, 16, 55, 2, 0, time.UTC)),
+						UpdatedAt:      timep(time.Date(2025, 7, 19, 11, 32, 13, 0, time.UTC)),
+						DomainNames:    []string{"translaser.com.br"},
+						Details:        "",
+						Notes:          "",
+						GroupId:        20488928016667,
+						Tags:           []string{"vendor", "reseller"},
+						OrganizationFields: &OrganizationFields{
+							MakerspaceDiscountCode: "",
+							OrgReseller:            false,
+							ResellerDiscountCode:   "",
+							SyncShopifyCompany:     false,
+						},
+					},
+					{
+						Url:            "https://lightburnsoftware.zendesk.com/api/v2/organizations/20489593231899.json",
+						Id:             20489593231899,
+						Name:           "3D Flying Bear",
+						SharedTickets:  false,
+						SharedComments: false,
+						ExternalId:     "",
+						CreatedAt:      timep(time.Date(2023, 11, 17, 21, 32, 53, 0, time.UTC)),
+						UpdatedAt:      timep(time.Date(2024, 2, 8, 22, 05, 24, 0, time.UTC)),
+						DomainNames:    []string{"3dflyingbear.com"},
+						Details:        "",
+						Notes:          "",
+						GroupId:        20488928016667,
+						Tags:           []string{"reseller", "vendor"},
+						OrganizationFields: &OrganizationFields{
+							MakerspaceDiscountCode: "",
+							OrgReseller:            true,
+							ResellerDiscountCode:   "Fly1ngB3@r-Di$tro",
+							SyncShopifyCompany:     false,
+						},
+					},
+				},
+			},
+		},
+		{
+			name:    "bad json",
+			data:    `[)`,
+			wanterr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := jsonToOrganizations([]byte(test.data))
+			if (err != nil) != test.wanterr {
+				t.Errorf("%s: error: got %v, did I want one? %v", test.name, err, test.wanterr)
+			}
+			if df := cmp.Diff(test.want, got); df != "" {
+				t.Errorf("%s: -want +got\n%s", test.name, df)
+			}
+		})
+	}
+}
